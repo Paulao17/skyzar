371def20086a007e634671eefacd8492ea0195f1
Paul ALNET <paul@shimaore.net>
2020-04-23 12:34:13 +0200

    Added crafts

--- crafting.js
+++ crafting.js
@@ -11,5 +11,205 @@ module.exports = {
   }, {
     craft: "ENCHANTED_REDSTONE_BLOCK",
     cost: 160 * 160
+  }],
+  ENCHANTED_REDSTONE: [{
+    craft: "ENCHANTED_REDSTONE_BLOCK",
+    cost: 160
+  }],
+  COAL: [{
+    craft: "ENCHANTED_COAL",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_COAL_BLOCK",
+    cost: 160 * 160
+  }],
+  ENCHANTED_COAL: [{
+    craft: "ENCHANTED_COAL_BLOCK",
+    cost: 160
+  }],
+  DIAMOND: [{
+    craft: "ENCHANTED_DIAMOND",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_DIAMOND_BLOCK",
+    cost: 160 * 160
+  }],
+  ENCHANTED_DIAMOND: [{
+    craft: "ENCHANTED_DIAMOND_BLOCK",
+    cost: 160
+  }],
+  IRON: [{
+    craft: "ENCHANTED_IRON",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_IRON_BLOCK",
+    cost: 160 * 160
+  }],
+  ENCHANTED_IRON: [{
+    craft: "ENCHANTED_IRON_BLOCK",
+    cost: 160
+  }],
+  GOLD: [{
+    craft: "ENCHANTED_GOLD",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_GOLD_BLOCK",
+    cost: 160 * 160
+  }],
+  ENCHANTED_GOLD: [{
+    craft: "ENCHANTED_GOLD_BLOCK",
+    cost: 160
+  }],
+  "INK_SACK:4": [{
+    craft: "ENCHANTED_LAPIS_LAZULI",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_LAPIS_LAZULI_BLOCK",
+    cost: 160 * 160
+  }],
+  ENCHANTED_LAPIS_LAZULI: [{
+    craft: "ENCHANTED_LAPIS_LAZULI_BLOCK",
+    cost: 160
+  }],
+  EMERALD: [{
+    craft: "ENCHANTED_EMERALD",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_EMERALD_BLOCK",
+    cost: 160 * 160
+  }],
+  ENCHANTED_EMERALD: [{
+    craft: "ENCHANTED_EMERALD_BLOCK",
+    cost: 160
+  }],
+  QUARTZ: [{
+    craft: "ENCHANTED_QUARTZ",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_QUARTZ_BLOCK",
+    cost: 160 * 160
+  }],
+  ENCHANTED_QUARTZ: [{
+    craft: "ENCHANTED_QUARTZ_BLOCK",
+    cost: 160
+  }],
+  GLOWSTONE_DUST: [{
+    craft: "ENCHANTED_GLOWSTONE_DUST",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_GLOWSTONE",
+    cost: 160 * 192
+  }],
+  ENCHANTED_GLOWSTONE_DUST: [{
+    craft: "ENCHANTED_GLOWSTONE",
+    cost: 192
+  }],
+  OBSIDIAN: [{
+    craft: "ENCHANTED_OBSIDIAN",
+    cost: 160
+  }],
+  FLINT: [{
+    craft: "ENCHANTED_FLINT",
+    cost: 160
+  }],
+  ICE: [{
+    craft: "ENCHANTED_ICE",
+    cost: 160
+  }],
+  ENCHANTED_ICE: [{
+    craft: "ENCHANTED_PACKED_ICE",
+    cost: 160
+  }],
+  PACKED_ICE: [{
+    craft: "ENCHANTED_ICE",
+    cost: 160 / 9 // 160 packed ice -> 9 enchanted ice
+  }],
+  SAND: [{
+    craft: "ENCHANTED_SAND",
+    cost: 160
+  }],
+  ENDSTONE: [{
+    craft: "ENCHANTED_ENDSTONE",
+    cost: 160
+  }],
+  WHEAT: [{
+    craft: "ENCHANTED_BREAD",
+    cost: 60
+  }, {
+    craft: "ENCHANTED_HAY_BLOCK",
+    cost: 9 * 9 * 16
+  }, {
+    craft: "HAY_BLOCK",
+    cost: 9
+  }],
+  HAY_BLOCK: [{
+    craft: "ENCHANTED_HAY_BLOCK",
+    cost: 9 * 16
+  }],
+  CARROT: [{
+    craft: "ENCHANTED_CARROT",
+    cost: 160
+  }],
+  POTATO_ITEM: [{
+    craft: "ENCHANTED_POTATO",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_BAKED_POTATO",
+    cost: 160 * 160
+  }],
+  PUMPKIN: [{
+    craft: "ENCHANTED_PUMPKIN",
+    cost: 160
+  }],
+  MELON: [{
+    craft: "ENCHANTED_MELON",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_MELON_BLOCK",
+    cost: 160 * 160
+  }],
+  ENCHANTED_MELON: [{
+    craft: "ENCHANTED_MELON_BLOCK",
+    cost: 160
+  }],
+  SEEDS: [{
+    craft: "ENCHANTED_SEEDS",
+    cost: 160
+  }],
+  RED_MUSHROOM: [{
+    craft: "ENCHANTED_RED_MUSHROOM",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_HUGE_MUSHROOM_2",
+    cost: 9 * 9 * 64
+  }, {
+    craft: "HUGE_MUSHROOM_2",
+    cost: 9
+  }],
+  HUGE_MUSHROOM_2: [{
+    craft: "ENCHANTED_HUGE_MUSHROOM_2",
+    cost: 9 * 64
+  }],
+  BROWN_MUSHROOM: [{
+    craft: "ENCHANTED_BROWN_MUSHROOM",
+    cost: 160
+  }, {
+    craft: "ENCHANTED_HUGE_MUSHROOM_1",
+    cost: 9 * 9 * 64
+  }, {
+    craft: "HUGE_MUSHROOM_2",
+    cost: 9
+  }],
+  HUGE_MUSHROOM_1: [{
+    craft: "ENCHANTED_HUGE_MUSHROOM_1",
+    cost: 9 * 64
+  }],
+  "INK_SACK:3": [{
+    craft: "ENCHANTED_COCOA",
+    cost: 160
+  }],
+  ENCHANTED_CACTUS_GREEN: [{
+    craft: "ENCHANTED_CACTUS",
+    cost: 160
   }]
 }
